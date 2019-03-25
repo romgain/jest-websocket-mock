781df0de426c3d1a91aaab28d96d9bf73a44a513
Romain Bertrand <romain@brtd.me>
2019-03-25 07:44:06 +0000

    Add ts support

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,15 @@
+import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import pkg from "./package.json";
 
 export default [
   {
     input: "src/index.js",
-    external: ["mock-socket"],
-    plugins: [babel()],
+    external: ["mock-socket", "jest-diff"],
+    plugins: [
+      resolve({ extensions: [".js", ".ts"] }),
+      babel({ extensions: [".js", ".ts"] }),
+    ],
     output: [
       { file: pkg.main, format: "cjs", exports: "named" },
       { file: pkg.module, format: "es" },
