33fd2f893112e53dea7f4e4aeb85043f4a50b751
Romain Bertrand <romain@brtd.me>
2019-03-26 18:18:24 +0000

    Migrate the websocket module to typescript

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,11 +1,21 @@
 import { Server } from "mock-socket";
 import Queue from "./queue";
 
-const identity = x => x;
+const identity = (x: string) => x;
+interface WSOptions {
+  jsonProtocol?: boolean;
+}
+export type DeserializedMessage = string | Object;
 
 export default class WS {
-  static instances = [];
-  messages = [];
+  server: Server;
+  connected: Promise<{}>;
+  closed: Promise<{}>;
+  serializer: (deserializedMessage: DeserializedMessage) => string;
+  deserializer: (message: string) => DeserializedMessage;
+
+  static instances: Array<WS> = [];
+  messages: Array<DeserializedMessage> = [];
   messagesToConsume = new Queue();
 
   static clean() {
@@ -16,13 +26,13 @@ export default class WS {
     WS.instances = [];
   }
 
-  constructor(url, { jsonProtocol = false } = {}) {
+  constructor(url: string, { jsonProtocol = false }: WSOptions = {}) {
     WS.instances.push(this);
 
     this.serializer = jsonProtocol ? JSON.stringify : identity;
     this.deserializer = jsonProtocol ? JSON.parse : identity;
 
-    let connectionResolver, closedResolver;
+    let connectionResolver: () => void, closedResolver!: () => void;
     this.connected = new Promise(done => (connectionResolver = done));
     this.closed = new Promise(done => (closedResolver = done));
 
@@ -33,7 +43,7 @@ export default class WS {
     this.server.on("connection", socket => {
       connectionResolver();
 
-      socket.on("message", message => {
+      socket.on("message", (message: string) => {
         const parsedMessage = this.deserializer(message);
         this.messages.push(parsedMessage);
         this.messagesToConsume.put(parsedMessage);
@@ -45,18 +55,18 @@ export default class WS {
     return this.messagesToConsume.get();
   }
 
-  send(message) {
+  send(message: DeserializedMessage) {
     this.server.emit("message", this.serializer(message));
   }
 
   close() {
-    this.server.emit("close");
+    this.server.emit("close", null);
     this.server.stop();
     this.server.close();
   }
 
   error() {
-    this.server.emit("error");
+    this.server.emit("error", null);
     this.server.close();
   }
 }
