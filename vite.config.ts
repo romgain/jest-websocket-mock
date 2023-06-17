b54e323af8dce016842d96ff7c95ad00335431f1
Akiomi Kamakura <akiomik@gmail.com>
2023-06-17 19:18:00 +0900

    Don't use vitest library mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,6 @@
-import nodePolyfills from 'vite-plugin-node-stdlib-browser';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [nodePolyfills()],
-  build: {
-    lib: {
-      entry: 'src/index.ts',
-      name: 'jest-websocket-mock',
-      fileName: (format) => `index.${format}.js`,
-    },
-  },
   test: {
     include: ['src/__tests__/**/*.test.{js,ts}'],
   },
